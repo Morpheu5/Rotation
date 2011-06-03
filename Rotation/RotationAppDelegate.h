c0a1e81930983e130eb78ded846be667bf067201
Andrea Franceschini <andrea.franceschini@gmail.com>
2011-06-03 12:16:42 +0200

    Initial Commit

---
+++ Rotation/RotationAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RotationAppDelegate.h
+//  Rotation
+//
+//  Created by Andrea Franceschini on 03/06/11.
+//  Copyright 2011 S/E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
